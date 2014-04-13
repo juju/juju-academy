a4a4e27dcb07d9fc5c67ab679cb2473bc50adbad
Marco Ceppi <marco@ceppi.net>
2014-04-12 22:18:03 -0400

    Now with 100% more dynamic loading

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,17 @@
+var greet = "Welcome to Juju Academy (juju-academy 0.0.1 web)\n\n\
+\
+ * Documentation:  https://help.juju.academy\n\n\
+\
+Last login: Sat Apr 12 15:34:53 2014"
+
+function load_lesson(l) {
+  $('.sidebar h1 .content').text(l['name']);
+  if(l['description'] instanceof Array) {
+    l['description'] = l['description'].join('</p><p>');
+  }
+  $('.sidebar .lesson.description').html('<p>' + l['description'] + '</p>');
+}
+
 $(document).ready(function() {
   //$('.filter.menu .item').tab();
   $('.ui.rating').rating({
@@ -7,15 +21,12 @@ $(document).ready(function() {
   // $('.ui.sidebar').sidebar('attach events', '.launch.button');
 
   $('.ui.dropdown').dropdown();
-});
 
-greet = "Welcome to Juju Academy (juju-academy 0.0.1 web)\n\n\
-\
- * Documentation:  https://help.juju.academy\n\n\
-\
-Last login: Sat Apr 12 15:34:53 2014"
+  $('.sidebar h1').click(function(e) {
+    $('.sidebar .lesson.description').slideToggle();
+    $('.sidebar h1 i').toggleClass('green');
+  });
 
-jQuery(function($) {
   $('#term').terminal(function(cmd, term) {
     if(cmd !== '' ) {
       term.echo('You typed a command, congrats');
@@ -24,4 +35,6 @@ jQuery(function($) {
     prompt: 'demo@ubuntu:~$ ',
     greetings: greet
   });
+
+  $.getScript("lessons/01-set-up-juju.jsonp");
 });
