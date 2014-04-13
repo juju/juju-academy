e6160bc96b401cda2025b71adfa834ac4964655e
Marco Ceppi <marco@ceppi.net>
2014-04-13 10:16:00 -0400

    wow. much lint. such hinting

--- assets/js/commands.js
+++ assets/js/commands.js
@@ -1,21 +1,33 @@
+if (!Function.prototype.bind) { // check if native implementation available
+  Function.prototype.bind = function(){
+    var fn = this, args = Array.prototype.slice.call(arguments),
+        object = args.shift();
+    return function(){
+      return fn.apply(object,
+        args.concat(Array.prototype.slice.call(arguments)));
+    };
+  };
+}
+
 var Commands = function() {
   this._cfg = {'commands': []};
   return this;
-}
+};
 
 Commands.prototype.add = function(match, priority) {
+  var runner;
   if(match instanceof String) {
-    var runner = function(cmd, term, next) {
+    runner = function(cmd, term, next) {
       var _m = match;
       match.match = _m;
       match.echo = "";
-    }
+    };
   }
 
   if($.isFunction(match)) {
-    var runner = match;
+    runner = match;
   } else {
-    var runner = function(cmd, term, next) {
+    runner = function(cmd, term, next) {
       if(!cmd.match(match.match)) {
         return next();
       }
@@ -28,16 +40,18 @@ Commands.prototype.add = function(match, priority) {
   }
 
   return this._cfg.commands.push(runner);
-}
+};
 
 Commands.prototype.run = function(cmd, term) {
   this._exec(cmd, term, 0);
-}
+};
 
 Commands.prototype._exec = function(cmd, term, key) {
   var self = this;
   this._cfg.commands[key](cmd, term, function() {
     key++;
-    self._exec(cmd, term, key);
+    if(key < self._cfg.commands.length) {
+      return self._exec(cmd, term, key);
+    }
   });
-}
+};
