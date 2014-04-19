7d155941ad715b7dfc3a5049bfa208fc07fda2ad
Marco Ceppi <marco@ceppi.net>
2014-04-19 12:35:08 -0400

    Add localstorage to files, now your data persists!

--- assets/js/files.js
+++ assets/js/files.js
@@ -2,20 +2,40 @@
 // Use localstorage to persist files between page loads/visits
 var Files = function() {
   this._files = {};
+  this.storage = $.localStorage;
+
   return this;
 };
 
 Files.prototype.exists = function(f) {
+  this.load();
   return (f in this._files);
 };
 
 Files.prototype.save = function(f, contents) {
   this._files[f] = contents;
+  this.sync();
 };
 
-Files.prototype.load = function(f) {
+Files.prototype.open = function(f) {
+  this.load();
   if(!this.exists(f)) {
     return "";
   }
   return this._files[f];
+};
+
+Files.prototype.sync = function() {
+  this.storage.set('fs', this._files);
+};
+
+Files.prototype.load = function() {
+  this._files = this.storage.get('fs');
+  if(!this._files) {
+    this._files = {};
+  }
+};
+
+Files.prototype.clear = function() {
+  this.storage.remove('fs');
 };
