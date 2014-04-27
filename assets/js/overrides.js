92b92344acf7e346428a2e6191a069592fbc9c7d
Marco Ceppi <marco@ceppi.net>
2014-04-27 10:53:18 -0400

    Added note about me knowing I'm doing bad things

--- assets/js/overrides.js
+++ assets/js/overrides.js
@@ -1,4 +1,15 @@
-if (!String.prototype.format) {
+
+/**
+ * Yes, I have read all the blog posts about not modifying objects you don't own
+ * These all replicate the ECMAScript 6 implementations. The idea being I'm tired
+ * of writing shitty code because web browsers are too slow to adopt new standards
+ * Some functions don't exist, but I want them to really badly (like format) so
+ * they are now in here. As new standards come out this overrides file (and
+ * subsequent code that uses these overrides) should be updated to fit any new
+ * ECMA specs
+ */
+
+if (typeof String.prototype.format != 'function') {
   String.prototype.format = function() {
     var args = arguments;
     return this.replace(/{(\d+)}/g, function(match, number) {
@@ -7,13 +18,14 @@ if (!String.prototype.format) {
   };
 }
 
-if (!Function.prototype.bind) { // check if native implementation available
-  Function.prototype.bind = function(){
-    var fn = this,
-        args = Array.prototype.slice.call(arguments),
-        object = args.shift();
-    return function() {
-      return fn.apply(object, args.concat(Array.prototype.slice.call(arguments)));
-    };
+if (typeof String.prototype.contains != 'function') {
+  String.prototype.contains = function(s) {
+    return this.indexOf(s) !== -1;
+  };
+}
+
+if (typeof String.prototype.startsWith != 'function') {
+  String.prototype.startsWith = function (s){
+    return this.indexOf(s) === 0;
   };
 }
